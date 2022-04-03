1837ca26f6cad7280212603d38fcfa357138d8a4
Uanderson S. Leonel <uanderson.leonel@viasoft.com.br>
2022-04-03 19:39:01 -0300

    Remove unused components

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useState } from 'react';
-import { Platform, Text, TouchableOpacity } from 'react-native';
+import { Platform } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
