74924e7712659899c401ddfe0c58f5a636056790
Uanderson S. Leonel <uanderson.leonel@viasoft.com.br>
2022-04-04 22:26:20 -0300

    Add SocialMediaButton

---
+++ src/components/SignInSocialButton/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { TouchableOpacityProps } from "react-native";
+import { SvgProps } from "react-native-svg";
+
+import {
+  Button,
+  ImageContainer,
+  Title,
+} from "./styles";
+
+interface Props extends TouchableOpacityProps {
+  title: string;
+  svg: React.FC<SvgProps>
+}
+
+export function SignInSocialButton({
+  title,
+  svg: Svg,
+  ...rest
+} : Props) {
+  return (
+    <Button {...rest}>
+      <ImageContainer>
+        <Svg />
+      </ImageContainer>
+      <Title>{title}</Title>
+    </Button>
+  )
+}
